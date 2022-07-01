ae8fa52bf6a227ae4bffcdfa77e9bfe0fd26fcab
MirzaAyon <ayonmirza98@gmail.com>
2022-07-01 23:56:00 +0600

    20 divs are showing on the UI

--- app6.js
+++ app6.js
@@ -16,9 +16,10 @@ const displayDog = (doglist) => {
     const first20data = doglist.slice(0, 20);
     // console.log(first20data);
     //20 ta data show kortese
-    const div = document.createElement('div');
+    // const div = document.createElement('div');
     // console.log(div);
     //hae inspact e div ta create hoise
+    //div take 1 bar create korle hbe na karon 20 ta data ache tai eke for loop er vitore create krbo
     for (const dog of first20data) {
         // console.log(dog);
         //inspact e prottekta data ke loop kore ber kore ante parsi
@@ -27,6 +28,7 @@ const displayDog = (doglist) => {
         //shob gula name show korse
 
         //ekhn div er modhe data gulake dekhabo
+        const div = document.createElement('div');
         div.innerHTML = `
         <h2>${dog.name}</h2>
         `
