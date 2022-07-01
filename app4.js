614d5806c03ee7fb9cff2ad06abb72a75eb0822d
MirzaAyon <ayonmirza98@gmail.com>
2022-07-01 22:13:04 +0600

    first 20 data and div is showing in the inspact

--- app4.js
+++ app4.js
@@ -8,7 +8,16 @@ const loadDog = () => {
 const displayDog = (doglist) => {
     // console.log(doglist);
     const main = document.getElementById('main');
-    console.log(main);
+    // console.log(main);
     //div ta inspact e peye gesi bhul thakle null show krto
+
+    //172 tar modhe 20 ta data nibo ei jonno slice use krbo
+
+    const first20data = doglist.slice(0, 20);
+    // console.log(first20data);
+    //20 ta data show kortese
+    const div = document.createElement('div');
+    // console.log(div);
+    //hae inspact e div ta create hoise
 
 }
